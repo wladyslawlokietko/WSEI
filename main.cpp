96cb471743cd393f0e6944b1159aaa2d1403ea85
bartm <bartminski.leszek@gmail.com>
2020-03-20 22:09:01 +0100

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int liczba, strzal, ile_prob=0;
+
+int main()
+
+    {
+
+
+
+        srand(time(NULL));
+        liczba = rand() % 100 + 1;
+
+        while (liczba != strzal)
+        {
+            ile_prob++;
+            cout << "Zgadnij liczbę: " << endl;
+            cin >> strzal;
+
+            if (liczba > strzal)
+                cout << "Za mało! Jeszcze raz: " << endl;
+            else if (liczba < strzal)
+                cout << "Za dużo! Jeszcze raz: " << endl;
+
+        }
+        cout << "Wygrałeś za " << ile_prob << " razem!" << endl;
+
+        system("pause");
+
+        return 0;
+    }
